9892abcd1b46dd3d25ad9e66ec4f129f4835e0eb
rohullahN <41601768+rohullahN@users.noreply.github.com>
2024-05-09 12:40:46 -0400

    add typeDefs and resolvers for users and transactions, merge the typeDefs and resolvers and add dummy data

---
+++ backend/resolvers/user.resolver.js
@@ -0,0 +1,11 @@
+import { users } from "../dummyData/data.js";
+const userResolver = {
+  Query: {
+    users: () => {
+      return users;
+    },
+  },
+  Mutation: {},
+};
+
+export default userResolver;
