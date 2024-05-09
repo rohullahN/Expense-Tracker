fe78d11673732709f50fc7bb735257220a72a704
rohullahN <41601768+rohullahN@users.noreply.github.com>
2024-05-09 13:18:47 -0400

    switch to express server from  standalone server

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,32 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+const app = express();
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
 
-console.log(`server ready ${url}`);
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -4,6 +4,9 @@ const userResolver = {
     users: () => {
       return users;
     },
+    user: (_, { userId }) => {
+      return users.find((user) => user._id === userId);
+    },
   },
   Mutation: {},
 };
